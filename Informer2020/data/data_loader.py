17862917cb3da2e9d3c1c58f57bbb550de9c8b12
w <zhengzilong275500@outlook.com>
2024-10-14 08:45:08 +0800

    修改了data_loader中datapath的位置

--- Informer2020/data/data_loader.py
+++ Informer2020/data/data_loader.py
@@ -14,7 +14,7 @@ warnings.filterwarnings('ignore')
 
 class Dataset_ETT_hour(Dataset):
     def __init__(self, root_path, flag='train', size=None, 
-                 features='S', data_path='ETTh1.csv', 
+                 features='S', data_path='2021.csv',
                  target='OT', scale=True, inverse=False, timeenc=0, freq='h', cols=None):
         # size [seq_len, label_len, pred_len]
         # info
--- Informer2020/results/readnpy.py
+++ Informer2020/results/readnpy.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-folder = 'informer_ETTh1_ftM_sl96_ll48_pl23_dm512_nh8_el2_dl1_df2048_atprob_fc5_ebtimeF_dtTrue_mxTrue_test_0'
+folder = 'informer_2021_ftM_sl96_ll48_pl24_dm512_nh8_el2_dl1_df2048_atprob_fc5_ebtimeF_dtTrue_mxTrue_test_0'
 pred = np.load(
-    'informer_2021_ftMS_sl96_ll48_pl24_dm512_nh8_el2_dl1_df2048_atprob_fc5_ebtimeF_dtTrue_mxTrue_test_1/pred.npy')
+    'informer_2021_ftM_sl96_ll48_pl24_dm512_nh8_el2_dl1_df2048_atprob_fc5_ebtimeF_dtTrue_mxTrue_test_1/pred.npy')
 true = np.load(
-    'informer_2021_ftMS_sl96_ll48_pl24_dm512_nh8_el2_dl1_df2048_atprob_fc5_ebtimeF_dtTrue_mxTrue_test_1/true.npy')
+    'informer_2021_ftM_sl96_ll48_pl24_dm512_nh8_el2_dl1_df2048_atprob_fc5_ebtimeF_dtTrue_mxTrue_test_1/true.npy')
 
 print(pred.shape)
 print(true.shape)
